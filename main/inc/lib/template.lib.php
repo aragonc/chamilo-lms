447f80df810202920b096ff47854297f9e5a5e64
Alex Aragon <aragcar@gmail.com>
2023-06-22 14:09:19 -0500

    add logo entity

--- main/inc/lib/template.lib.php
+++ main/inc/lib/template.lib.php
@@ -738,6 +738,15 @@ class Template
             $this->assign('css_style_print', $style_print);
         }
 
+        if (api_get_plugin_setting('proikos', 'tool_enable') === 'true') {
+            $plugin = ProikosPlugin::create();
+            $code_reference = $plugin->getCodeReferenceByUser(api_get_user_id());
+            $picture = null;
+            if($code_reference != 'ALL'){
+                $picture = $plugin->getPictureEntity($code_reference);
+            }
+            $this->assign('entity_proikos', $picture);
+        }
         // Logo
         $logo = return_logo($this->theme);
         $logoPdf = return_logo($this->theme, false);
